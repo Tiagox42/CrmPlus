f26ffdf758551b7992acd4620f336a6e2d511a0e
Tiago Dantas <tiagodantas42@gmail.com>
2021-12-06 00:42:33 -0300

    large update

--- bugger.js
+++ bugger.js
@@ -1,72 +1,100 @@
 let changeColor = document.getElementById("startBugger");
 
 changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: executarBugger,
-  });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: executarBugger,
+    });
 });
 
 function executarBugger() {
-    setTimeout(() => {
 
-        let colorA = '#ba4343';
-        let colorC = '#5ad15a';
-        let colorT = '#ffc64a';
-        let colorSafe = '#4afff9';
-        let colorS = '#bd10e8';
-        let colorA2 = '#4b48db';
-    
-        let contagem = document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div").children;
-        console.log("A contagem é "+contagem.length);
-    
-        for (var i = 1; i < contagem.length+1; i++) {
-    
-            if (document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child("+i+") > div:nth-child(8) > div > div > div > div > div > div.ms-StackItem.truncatableText-217 > label").textContent == "Aguardando Cliente") {
-    
-                document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child("+i+")").style.backgroundColor = colorC;   
-            }
-            
-            else if (document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child("+i+") > div:nth-child(8) > div > div > div > div > div > div.ms-StackItem.truncatableText-217 > label").textContent == "Em Atendimento") {
-    
-                document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child("+i+")").style.backgroundColor = colorA;   
-            } 
-            
-            else if (document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child("+i+") > div:nth-child(8) > div > div > div > div > div > div.ms-StackItem.truncatableText-217 > label").textContent == "Aguardando Terceiros") {
-    
-                document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child("+i+")").style.backgroundColor = colorT;   
-            }
-    
-            else if (document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child("+i+") > div:nth-child(8) > div > div > div > div > div > div.ms-StackItem.truncatableText-217 > label").textContent == "Aguardando Atendimento") {
-    
-                document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child("+i+")").style.backgroundColor = colorSafe;   
-            }
-    
-         }
-    
-    
-        for (var i = 1; i < contagem.length+1; i++) {
-    
-            if (document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child("+i+") > div:nth-child(9) > div > div > div > div > div > div.ms-StackItem.truncatableText-217 > label").textContent == "4 - Solucionar") {
-    
-                document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child("+i+") > div:nth-child(9) > div > div > div > div > div > div.ms-StackItem.truncatableText-217 > label").style.backgroundColor = colorS;
-            }
-    
-            else if (document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child("+i+") > div:nth-child(9) > div > div > div > div > div > div.ms-StackItem.truncatableText-217 > label").textContent == "2 - Aguard. Atendimento - 2º nível") {
-    
-                document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child("+i+")").style.backgroundColor = colorA2;
-            }
-    
-         }
-         var button = document.querySelector("#sitemap-entity-New_ocorrencia > div > div > div.pa-bi.pa-e.pa-o.pa-cj.flexbox");
-         var button2 = document.querySelector("#incident\\|NoRelationship\\|HomePageGrid\\|Mscrm\\.Modern\\.refreshCommand30-button");
-         button.onclick = function() {
-            start();
-        };
-        button2.onclick = function() {
-            start();
-        };
-        }, 1500);   
+    chrome.storage.sync.get([
+
+        'data1',
+        "data2",
+        "data3",
+        "data4",
+        "data5",
+        "data6",
+        "data7",
+
+    ], function (result) {
+
+        let colorA = result.data1;
+        let colorC = result.data2;
+        let colorT = result.data3;
+        let colorSafe = result.data4;
+        let colorS = result.data5;
+        let colorA2 = result.data6;
+        let colorM = result.data7;
+        let colorBlack = '#262626';
+
+            setTimeout(() => {
+
+                let contagem = document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div").children;
+
+                for (var i = 1; i < contagem.length + 1; i++) {
+
+                    if (document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child(" + i + ") > div:nth-child(8) > div > div > div > div > div > div.ms-StackItem.truncatableText-217 > label").textContent == "Aguardando Cliente") {
+
+                        document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child(" + i + ")").style.backgroundColor = colorC;
+                    }
+
+                    else if (document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child(" + i + ") > div:nth-child(8) > div > div > div > div > div > div.ms-StackItem.truncatableText-217 > label").textContent == "Em Atendimento") {
+
+                        document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child(" + i + ")").style.backgroundColor = colorA;
+                    }
+
+                    else if (document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child(" + i + ") > div:nth-child(8) > div > div > div > div > div > div.ms-StackItem.truncatableText-217 > label").textContent == "Aguardando Terceiros") {
+
+                        document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child(" + i + ")").style.backgroundColor = colorT;
+                    }
+
+                    else if (document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child(" + i + ") > div:nth-child(8) > div > div > div > div > div > div.ms-StackItem.truncatableText-217 > label").textContent == "Aguardando Atendimento") {
+
+                        document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child(" + i + ")").style.backgroundColor = colorSafe;
+                    }
+
+                }
+
+
+                for (var i = 1; i < contagem.length + 1; i++) {
+
+                    document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child(" + i + ")").style.borderColor = colorBlack;
+
+                    if (document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child(" + i + ") > div:nth-child(9) > div > div > div > div > div > div.ms-StackItem.truncatableText-217 > label").textContent == "4 - Solucionar") {
+
+                        document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child(" + i + ") > div:nth-child(9)").style.backgroundColor = colorS;
+                    }
+
+                    else if (document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child(" + i + ") > div:nth-child(9) > div > div > div > div > div > div.ms-StackItem.truncatableText-217 > label").textContent == "2 - Aguard. Atendimento - 2º nível") {
+
+                        document.querySelector("#entity_control-pcf_grid_control_container > div > div.pcf-grid.root-142 > div > div > div > div > div.ag-root-wrapper-body.ag-layout-normal.ag-focus-managed > div.ag-root.ag-unselectable.ag-layout-normal > div.ag-body-viewport.ag-layout-normal.ag-row-no-animation > div.ag-center-cols-clipper > div > div > div:nth-child(" + i + ")").style.backgroundColor = colorA2;
+                    }
+
+                }
+                var button = document.querySelector("#sitemap-entity-New_ocorrencia > div > div > div.pa-bi.pa-e.pa-o.pa-cj.flexbox");
+                var button2 = document.querySelector("#incident\\|NoRelationship\\|HomePageGrid\\|Mscrm\\.Modern\\.refreshCommand30-button");
+                button.onclick = function () {
+                    start();
+                };
+                button2.onclick = function () {
+                    start();
+                };
+
+            }, 1500);
+
+
+        }
+        );
+
+
+
+
+        console.log("[CRM-PLUS] Bugger aplicado");
+
+
 }
--- saver.js
+++ saver.js
@@ -1,30 +1,66 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("DOM completamente carregado e analisado");
-    document.getElementById("save").addEventListener("click", save);
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.getElementById("save").addEventListener("click", save);
+  document.getElementById("padrao").addEventListener("click", padrao);
+
+  chrome.storage.sync.get([
+
+    'data1',
+    "data2",
+    "data3",
+    "data4",
+    "data5",
+    "data6",
+    "data7",
+
+  ], function (result) {
+
+    document.getElementById('colorA').value = result.data1;
+    document.getElementById('colorC').value = result.data2;
+    document.getElementById('colorT').value = result.data3;
+    document.getElementById('colorSafe').value = result.data4;
+    document.getElementById('colorS').value = result.data5;
+    document.getElementById('colorA2').value = result.data6;
+    document.getElementById('colorM').value = result.data7;
+
+    console.log("[CRM-PLUS] Cores antigas aplicadas");
   });
+
+});
 
 function save() {
 
-    var colorA = document.getElementById('colorA').value;
-    var colorC = document.getElementById('colorC').value;
-    var colorT = document.getElementById('colorT').value;
-    var colorSafe = document.getElementById('colorSafe').value;
-    var colorS = document.getElementById('colorS').value;
-    var colorA2 = document.getElementById('colorA2').value;
-    var colorM = document.getElementById('colorM').value;
+  var colorA = document.getElementById('colorA').value;
+  var colorC = document.getElementById('colorC').value;
+  var colorT = document.getElementById('colorT').value;
+  var colorSafe = document.getElementById('colorSafe').value;
+  var colorS = document.getElementById('colorS').value;
+  var colorA2 = document.getElementById('colorA2').value;
+  var colorM = document.getElementById('colorM').value;
 
+  chrome.storage.sync.set({
+    "data1": colorA,
+    "data2": colorC,
+    "data3": colorT,
+    "data4": colorSafe,
+    "data5": colorS,
+    "data6": colorA2,
+    "data7": colorM
 
+  }, function () {
+    
+  });
 
-    chrome.storage.sync.set({ 
+  alert('Configurações salvas');
+}
 
-        colorA:  colorA,
-        colorC: colorC,
-        colorT: colorT,
-        colorSafe: colorSafe,
-        colorS: colorS,
-        colorA2: colorA2,
-        colorM: colorM
-      
-    });
-    alert('Configurações salvas');
-  }
+function padrao() {
+
+  document.getElementById('colorA').value = '#ba4343';
+  document.getElementById('colorC').value = '#5ad15a';
+  document.getElementById('colorT').value = '#ffc64a';
+  document.getElementById('colorSafe').value = '#4afff9';
+  document.getElementById('colorS').value = '#bd10e8';
+  document.getElementById('colorA2').value = '#4b48db';
+  document.getElementById('colorM').value = '#8c8c8c';
+
+}
